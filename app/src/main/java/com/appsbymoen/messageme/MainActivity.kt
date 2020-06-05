b00ac5a035ff1fb8250d72b06600a95ce363c41d
Adil <adil.moen@gmail.com>
2020-06-05 12:26:50 -0400

    changed login screen layout

--- app/src/main/java/com/appsbymoen/messageme/MainActivity.kt
+++ app/src/main/java/com/appsbymoen/messageme/MainActivity.kt
@@ -2,10 +2,20 @@ package com.appsbymoen.messageme
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val email = editTextTextEmailAddress.text.toString()
+        val password = editTextTextPassword.text.toString()
+
+        Log.d("MainActivity", "Email is:" + email)
+        Log.d("MainActivity", "Password is:$password")
+
     }
 }
