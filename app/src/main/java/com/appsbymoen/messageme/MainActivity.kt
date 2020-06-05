2f38e23dab02a43544138cb6c0ae1b784fe339f4
Adil <adil.moen@gmail.com>
2020-06-05 15:01:52 -0400

    access input

--- app/src/main/java/com/appsbymoen/messageme/MainActivity.kt
+++ app/src/main/java/com/appsbymoen/messageme/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.appsbymoen.messageme
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
+import android.widget.Toast.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("ShowToast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -17,11 +21,21 @@ class MainActivity : AppCompatActivity() {
 
             val email = editTextTextEmailAddress.text.toString()
             val password = editTextTextPassword.text.toString()
+            val user =editTextTextPersonName.text.toString()
 
+            Log.d("MainActivity", "Name is:" + user )
             Log.d("MainActivity", "Email is:" + email)
             Log.d("MainActivity", "Password is:$password")
 
+            val t = makeText(this, "user: $user Email: $email PW: $password", LENGTH_LONG)
+            t.show()
         }
+
+        tvAlreadyHaveAccount.setOnClickListener {
+           val t = makeText(this, "You have account clicked!", LENGTH_LONG)
+            t.show()
+        }
+
 
     }
 }
